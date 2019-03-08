c5a4a8c33eefe15ab52150fbf3d031148957c29e
Will Grauvogel <builtbywill@gmail.com>
2019-03-08 07:10:40 -0500

    add backwards compatible export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-export { default as NET_ACTION } from './actions'
+import NET_ACTION from './actions'
+export { NET_ACTION }
+// deprecated, backwards compatible export
+export { NET_ACTION as actions }
+
 export * from './types'
 
 import fetchReducer from './fetchReducer'
 import fetchSaga from './fetchSaga'
 import noStoreSaga, { registerNoStoreActionHook, unregisterNoStoreActionHook } from './noStoreSaga'
-
 const reducers = { fetchReducer }
 const sagas = { fetchSaga, noStoreSaga }
 const hooks = { registerNoStoreActionHook, unregisterNoStoreActionHook }
